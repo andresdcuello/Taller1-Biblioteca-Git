8521658d992f0e108f55507cf75e695a71b490b4
Andres Daniel Cuello Blanco <andresdcuello@unicesar.edu.co>
2026-08-15 16:56:24 -0500

    feat: implementar CREATE de Cliente

--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -1,16 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.biblioteca;
 
-/**
- *
- * @author MSI KATANA
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    static ArrayList<Client> clients = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Aquí irá el menú (Fase 8)
     }
+    public static void createClient() {
+    System.out.println("--- Register New Client ---");
+    System.out.print("ID: ");
+    String id = sc.nextLine();
+    System.out.print("Name: ");
+    String name = sc.nextLine();
+    System.out.print("Phone: ");
+    String phone = sc.nextLine();
+    System.out.print("Email: ");
+    String email = sc.nextLine();
+
+    Client client = new Client(id, name, phone, email);
+    clients.add(client);
+    System.out.println("Client successfully registered.");
+}
 }
